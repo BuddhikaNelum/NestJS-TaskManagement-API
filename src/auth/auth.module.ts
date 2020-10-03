6eb38e6240f4c6c63e73184390613103d4c58d79
= <=>
2020-10-04 01:43:23 +0530

    completed authentication with jwt and passport.js

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
+  imports: [
+    PassportModule.register({
+      defaultStrategy: 'jwt'
+    }),
+    JwtModule.register({
+      secret: 'topsecrect51',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    TypeOrmModule.forFeature([UserRepository])
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule]
 })
 export class AuthModule {}
